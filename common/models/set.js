241b0e48ccb5c75b0e4a38e037d4c667734eefba
Martin Melcher <martin@inclumedia.at>
2015-12-27 23:44:02 +0100

    getUpdates endpoint added

--- common/models/set.js
+++ common/models/set.js
@@ -1,4 +1,50 @@
 module.exports = function(Set) {
+
+  Set.getUpdates = function(countryCode, code, lastSynced, cb) {
+
+    var fObj = {
+      where: {
+        and: {
+          countryCode: countryCode,
+          code: code,
+          or: {
+            created: {
+              gt: lastSynced
+            },
+            updated: {
+              gt: lastSynced
+            }
+          }
+        }
+      }
+    };
+
+    Set.find(fObj, function(err, sets){
+      if (err) {
+        Set.app.winston.log('warning', "Set find operation failed.",
+          {"msg": err.message, "stack": err.stack});
+        return cb(err);
+      }
+      return cb(null, sets);
+    });
+  };
+
+  Set.remoteMethod(
+    'getUpdates',
+    {
+      accepts: [
+        {arg: 'countryCode', type: 'string', required: true},
+        {arg: 'code', type: 'string', required: true},
+        {arg: 'lastSynced', type: 'date', required: true}
+      ],
+      returns: [
+        {type: 'array', root: true}
+      ],
+      http: {path: '/updated', verb: 'post'},
+      description: "Gets sets which were updated since the last refresh."
+    }
+  );
+
   Set.disableRemoteMethod('create', true);
   Set.disableRemoteMethod('upsert', true);
   Set.disableRemoteMethod('deleteById', true);
